6c22e42ad8b26cd666bdecf0b68515c1a4088138
Nosov Konstantin <nosov@nodeart.io>
2025-12-27 16:23:57 +0100

    add /privacy /terms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<footer>
+	<a href={resolve('/privacy')}>Privacy</a> | <a href={resolve('/terms')}>Terms</a>
+</footer>
